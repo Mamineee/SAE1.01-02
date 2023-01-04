40bc2b1bde5b1e2e2b798499baab748a2581080a
Mohamed-Amin <mamine22123@gmail.com>
2023-01-04 17:01:14 +0100

    srh

---
+++ ressources/niveauDepart/niveaudepart.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="niveauDepart" tilewidth="8" tileheight="8" tilecount="96" columns="12">
+ <image source="zone de depart.png" width="100" height="64"/>
+ <tile id="75">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.09091" y="14.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.09091" y="14.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.54545" y="15.1818"/>
+  </objectgroup>
+ </tile>
+</tileset>
