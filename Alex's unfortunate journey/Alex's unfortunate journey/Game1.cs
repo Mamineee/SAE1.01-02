7394b874d4dd19b5e8453924ead9e5e3d88bb2b3
Mohamed-Amin <mamine22123@gmail.com>
2023-01-05 09:40:04 +0100

    affichage perso

--- Alex's unfortunate journey/Alex's unfortunate journey/Game1.cs
+++ Alex's unfortunate journey/Alex's unfortunate journey/Game1.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+//using MonoGame.Extended.Animations;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
+
 
 namespace Alex_s_unfortunate_journey
 {
@@ -13,6 +18,10 @@ namespace Alex_s_unfortunate_journey
         //map
         private TiledMap _tiledMap;
         private TiledMapRenderer _tiledMapRenderer;
+
+        //perso
+        private Vector2 _positionPerso;
+        private AnimatedSprite _persoIdle;
 
         public Game1()
         {
@@ -28,10 +37,9 @@ namespace Alex_s_unfortunate_journey
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
             _graphics.PreferredBackBufferWidth = 1200;
             _graphics.PreferredBackBufferHeight = 768;
-           _graphics.ApplyChanges();
-
-
-
+            _graphics.ApplyChanges();
+            //perso
+            _positionPerso = new Vector2(304,624);
 
             base.Initialize();
         }
@@ -42,6 +50,9 @@ namespace Alex_s_unfortunate_journey
             //map
             _tiledMap = Content.Load<TiledMap>("niveauDepart2");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            //perso
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("MC_idle_Right.sf", new JsonContentLoader());
+            _persoIdle = new AnimatedSprite(spriteSheet);
             // TODO: use this.Content to load your game content here
         }
 
@@ -49,9 +60,22 @@ namespace Alex_s_unfortunate_journey
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             //map
             _tiledMapRenderer.Update(gameTime);
-            // TODO: Add your update logic here
+            //perso
+            _persoIdle.Play("mC_Idle_Right");
+            _persoIdle.Update(deltaTime);
+
+            if (_keyboardState.IsKeyDown(Keys.D))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 1);
+                _animation = "walkEast";
+                _positionPerso.X += walkSpeed;
+                if (IsCollision(tx, ty))
+                    _positionPerso.X -= walkSpeed;
+            }
 
             base.Update(gameTime);
         }
@@ -63,6 +87,10 @@ namespace Alex_s_unfortunate_journey
             // TODO: Add your drawing code here
             //map
             _tiledMapRenderer.Draw();
+            //perso
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_persoIdle, _positionPerso);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
